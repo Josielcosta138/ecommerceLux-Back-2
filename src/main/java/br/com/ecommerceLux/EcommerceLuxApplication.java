d012c065c8200f92b89bfcfc3a5f354cc0c8f4d7
Josielcosta138 <63210548+Josielcosta138@users.noreply.github.com>
2024-07-10 20:29:36 -0300

    config manter aplicação no ar

--- src/main/java/br/com/ecommerceLux/EcommerceLuxApplication.java
+++ src/main/java/br/com/ecommerceLux/EcommerceLuxApplication.java
@@ -2,8 +2,10 @@ package br.com.ecommerceLux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EcommerceLuxApplication {
 
 	public static void main(String[] args) {
